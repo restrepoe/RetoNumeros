4615a0d857c9cd9ab9e34aafd028694a564cbdf6
Esteban Restrepo Sanchez <restrepoe1992@gmail.com>
2016-08-18 16:54:59 -0500

    1

--- RetoNumeros/ViewController.swift
+++ RetoNumeros/ViewController.swift
@@ -12,36 +12,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-var bingo = 0...100
-    
-for numero in bingo{
-    
-    var numeroPar = numero  % 2
-    var numeroEnCinco = numero % 5
-    var numeroImpar = numero % 1
-    
-    if numero > 29 && numero < 41{
-    print(numero, "Viva Swift!!!")
-    }else if numeroEnCinco == 0{
-    print(numero, "Bingo")
-    }else if numeroPar == 0{
-    print(numero, "Par")
-    }else if numeroImpar == 0{
-    print(numero, "Impar")
-    }
-    
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
